858751d189099e2c58ea8d8c0c35bdee96cd5d37
Rudi <rudi@e-ld.de>
2025-01-26 07:54:18 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Support;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Using the data from the decompiled file: 
+        string passwordEncodedAsBase64 = "0Nv32PTwgYjzg9/8j5TbmvPd3e7WhtWWyuPsyO76/Y+U193E";
+        byte[] key = Encoding.ASCII.GetBytes("armando");
+
+        byte[] passwordBase64AsByte = Convert.FromBase64String(passwordEncodedAsBase64);
+        byte[] passwordAsByte = passwordBase64AsByte;
+        for (int i = 0; i < passwordBase64AsByte.Length; i++)
+        {
+            passwordAsByte[i] = (byte)((uint)(passwordBase64AsByte[i] ^ key[i % key.Length]) ^ 0xDFu);
+        }
+        string passwordDecoded = Encoding.UTF8.GetString(passwordAsByte);
+        Console.WriteLine($"Decoded password: {passwordDecoded}");
+    }
+}
